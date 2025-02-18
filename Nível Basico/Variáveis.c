3338221ba4e4a41c6e91b9e3148420c3dbc6cd01
pereiraimpera <eronpereira12_4@outlook.com>
2025-02-18 23:12:05 +0000

    conclusão nível Basico

---
+++ Nível Basico/Variáveis.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 26;
+    int quantidade = 1;
+    float altura = 1.75;
+    double peso = 63.3;
+    char opçao ='E';
+    char nome[20] = "Eron";
+
+    
+
+}
