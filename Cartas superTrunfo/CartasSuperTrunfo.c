10ea96cc3c67e3a22d8d6efee7271656a69c715c
pereiraimpera <eronpereira12_4@outlook.com>
2025-02-19 22:58:19 +0000

    Criação Carta 1

---
+++ Cartas superTrunfo/CartasSuperTrunfo.c
@@ -0,0 +1,39 @@
+# include <stdio.h>
+
+int main(){
+
+    //Variáveis Carta 1
+     int populacao, turisticos;
+     float area, pib;
+     char est;
+     char codcarta[20];
+     char cidade[50];
+
+     //cadastrando Carta 1
+     printf("Carta Número 1\n");
+     printf("Digite a letra do Estado: ");
+     scanf("%s", &est); //inserindo letra do estado
+     printf("Digite o Código da Carta: ");
+     scanf("%s", &codcarta); //código da carta
+     printf("Digite o Nome da Cidade: ");
+     scanf("%s", &cidade); //Nome da Cidade
+     printf("Digite a População da Cidade: ");
+     scanf("%d", &populacao); // População
+     printf("Digite a Área (em Km²): ");
+     scanf("%f", &area); // Área em km²
+     printf("Digite o PIB: ");
+     scanf("%f", &pib); // Inserindo PIB
+     printf("Número de Pontos Turísticos: ");
+     scanf("%d", &turisticos); // Pontos Turísticos
+
+
+
+
+
+
+
+
+
+
+
+}
